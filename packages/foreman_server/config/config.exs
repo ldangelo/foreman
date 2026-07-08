4740ec9c471303fde00d2fd7d3f8cbaae160a4a0
Leo A. D'Angelo <ldangelo@mac.com>
2026-07-08 13:10:52 -0500

    fix: isolate ecto migration table

--- packages/foreman_server/config/config.exs
+++ packages/foreman_server/config/config.exs
@@ -4,6 +4,7 @@ config :foreman_server,
   ecto_repos: [ForemanServer.Repo]
 
 config :foreman_server, ForemanServer.Repo,
-  pool_size: String.to_integer(System.get_env("FOREMAN_SERVER_DB_POOL_SIZE") || "10")
+  pool_size: String.to_integer(System.get_env("FOREMAN_SERVER_DB_POOL_SIZE") || "10"),
+  migration_source: "foreman_server_schema_migrations"
 
 import_config "#{config_env()}.exs"
