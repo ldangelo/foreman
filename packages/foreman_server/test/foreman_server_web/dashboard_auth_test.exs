638f349716e251c5b4080bd46ff4316386f4e039
Leo A. D'Angelo <ldangelo@mac.com>
2026-08-18 19:59:28 -0500

    test(JLD-T004): dashboard auth test per TRD-2026-4212be7e

---
+++ packages/foreman_server/test/foreman_server_web/dashboard_auth_test.exs
@@ -0,0 +1,7 @@
+defmodule ForemanServerWeb.DashboardAuthTest do
+  use ForemanServerWeb.ConnCase, async: false
+  test "unauthenticated request gets redirect or 401" do
+    conn = build_conn() |> get("/dashboard")
+    assert conn.status in [302, 401, 403]
+  end
+end
