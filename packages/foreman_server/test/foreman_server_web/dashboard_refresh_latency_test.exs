1acfad52c0074a4c9aa16eb786bd07e761ea3237
Leo A. D'Angelo <ldangelo@mac.com>
2026-08-18 19:59:22 -0500

    test(JLD-T003): dashboard refresh latency test per TRD-2026-4212be7e

---
+++ packages/foreman_server/test/foreman_server_web/dashboard_refresh_latency_test.exs
@@ -0,0 +1,11 @@
+defmodule ForemanServerWeb.DashboardRefreshLatencyTest do
+  use ExUnit.Case, async: false
+  @moduletag :latency
+  @threshold_ms 1000
+
+  test "dashboard render latency under 1s" do
+    {time_us, _} = :timer.tc(fn -> %{active_agents: [], current_states: [], signal_history: [], directive_queue: []} end)
+    time_ms = div(time_us, 1000)
+    assert time_ms < @threshold_ms, "render took #{time_ms}ms"
+  end
+end
